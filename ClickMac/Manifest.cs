silasary/clickmono

commit 8031a5806338603adf600642dd485654cc3b58b9

Groundwork refactor for verifying SHA256 signatures.

ClickMac/Manifest.cs
@@ -231,21 +231,8 @@ namespace ClickMac
             }
             catch (IOException) { }
             string filename = Path.Combine(".", version, Path.GetFileName(codebase));
-            bool downloaded = false;
-            if (File.Exists(filename))
-            {
-                if (new FileInfo(filename).Length == int.Parse(dependentAssembly.Attribute("size").Value)) // HACK: Not an actual equality test. (Although it's usually good enough)
-                {
-                    downloaded = true;
-                }
-                else
-                {
-                    if (File.Exists(filename + "._"))
-                        File.Delete(filename + "._");
-                    File.Move(filename, filename + "._");
-                }
-            }
-            if (!downloaded)
+            bool mustDownload = !VerifyExistingFile(dependentAssembly, filename);
+            if (mustDownload)
             {
                 Loading.Log("Getting Dependency {0}", codebase);
                 DownloadFile(path, codebase, filename);
@@ -266,6 +253,38 @@ namespace ClickMac
             }
         }
 
+        private static bool VerifyExistingFile(XElement dependentAssembly, string filename)
+        {
+            bool mustDownload = false;
+            if (!File.Exists(filename))
+            {
+                mustDownload = true;
+            }
+            else
+            {
+                int size = int.Parse(dependentAssembly.Attribute("size").Value);
+                string digestValue = dependentAssembly.Element(Namespace.XName("hash", ns.asmv2)).Element(Namespace.XName("DigestValue", ns.dsig)).Value;
+                string digestMethod = dependentAssembly.Element(Namespace.XName("hash", ns.asmv2)).Element(Namespace.XName("DigestMethod", ns.dsig)).Attribute("Algorithm").Value.Split('#')[1];
+                if (new FileInfo(filename).Length != size) // HACK: Not an actual equality test. (Although it's usually good enough)
+                {
+                    mustDownload = true;
+                }
+                if (!Crypto.AreEqual(filename, digestMethod, digestValue))
+                {
+                    mustDownload = true;
+                }
+
+                if (mustDownload)
+                {
+                    if (File.Exists(filename + "._"))
+                        File.Delete(filename + "._");
+                    File.Move(filename, filename + "._");
+                }
+            }
+
+            return mustDownload;
+        }
+
         private void GetFile(XElement file, string path)
         {
 
@@ -274,16 +293,7 @@ namespace ClickMac
             if (!Directory.Exists(Path.GetDirectoryName(filename)))
                 Directory.CreateDirectory(Path.GetDirectoryName(filename));
             Console.WriteLine("Getting {0}", filename);
-            bool downloaded = false;
-            if (File.Exists(filename))
-            {
-                if (new FileInfo(filename).Length == int.Parse(file.Attribute("size").Value))
-                {
-                    downloaded = true;
-                }
-                else
-                    File.Move(filename, filename + "._");
-            }
+            bool downloaded = VerifyExistingFile(file, filename);
             if (!downloaded)
             {
                 Directory.CreateDirectory(Path.GetDirectoryName(filename));

ClickMac/ClickMono.csproj
@@ -86,6 +86,7 @@
     <Reference Include="System.Xml" />
   </ItemGroup>
   <ItemGroup>
+    <Compile Include="Crypto.cs" />
     <Compile Include="CurlWrapper.cs" />
     <Compile Include="Namespace.cs" />
     <Compile Include="PreLoading.cs" />

ClickMac/Crypto.cs
@@ -1,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickMac
+{
+    public class Crypto
+    {
+        public static string GetSha256DigestValue(FileInfo file)
+        {
+            using (var stream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var sha = SHA256.Create())
+                {
+                    var bytes = sha.ComputeHash(stream);
+                    return Convert.ToBase64String(bytes);
+                }
+            }
+        }
+
+        public static string GetSha256DigestValue(byte[] data)
+        {
+            using (var sha = SHA256.Create())
+            {
+                var bytes = sha.ComputeHash(data);
+                return Convert.ToBase64String(bytes);
+            }
+        }
+
+        internal static bool AreEqual(string filename, string digestMethod, string digestValue)
+        {
+            // HACK: Totally not lying.
+            return true;
+        }
+    }
+}

Packager/Packager.csproj
@@ -64,9 +64,7 @@
     <None Include="App.config" />
   </ItemGroup>
   <ItemGroup>
-    <Content Include="package.ico">
-      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
-    </Content>
+    <Content Include="package.ico" />
   </ItemGroup>
   <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
   <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 

commit 23d8dc58962e5ed45ce7a66979a4e8bb14f01740

Fixed regression introduced in 8031a58

ClickMac/Manifest.cs
@@ -265,10 +265,10 @@ namespace ClickMac
 
         private static bool VerifyExistingFile(XElement dependentAssembly, string filename)
         {
-            bool mustDownload = false;
+            bool isInvalid = false;
             if (!File.Exists(filename))
             {
-                mustDownload = true;
+                isInvalid = true;
             }
             else
             {
@@ -277,14 +277,14 @@ namespace ClickMac
                 string digestMethod = dependentAssembly.Element(Namespace.XName("hash", ns.asmv2)).Element(Namespace.XName("DigestMethod", ns.dsig)).Attribute("Algorithm").Value.Split('#')[1];
                 if (new FileInfo(filename).Length != size) // HACK: Not an actual equality test. (Although it's usually good enough)
                 {
-                    mustDownload = true;
+                    isInvalid = true;
                 }
                 if (!Crypto.AreEqual(filename, digestMethod, digestValue))
                 {
-                    mustDownload = true;
+                    isInvalid = true;
                 }
 
-                if (mustDownload)
+                if (isInvalid)
                 {
                     if (File.Exists(filename + "._"))
                         File.Delete(filename + "._");
@@ -292,7 +292,7 @@ namespace ClickMac
                 }
             }
 
-            return mustDownload;
+            return isInvalid;
         }
 
         private void GetFile(XElement file, string path)
@@ -303,8 +303,8 @@ namespace ClickMac
             if (!Directory.Exists(Path.GetDirectoryName(filename)))
                 Directory.CreateDirectory(Path.GetDirectoryName(filename));
             Console.WriteLine("Getting {0}", filename);
-            bool downloaded = VerifyExistingFile(file, filename);
-            if (!downloaded)
+            bool invalid = VerifyExistingFile(file, filename);
+            if (invalid)
             {
                 Directory.CreateDirectory(Path.GetDirectoryName(filename));
                 try