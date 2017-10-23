519ea2e3b88cab7fd4ab14c232321c94c5976eb4
Katelyn Gigante <clockwork.singularity@gmail.com>
2017-10-23 14:47:25 +1100

    Add support for md5 :/

--- ClickMac/Common/Crypto.cs
+++ ClickMac/Common/Crypto.cs
@@ -48,6 +48,27 @@ namespace ClickMono.Common
             }
         }
 
+        public static string GetMd5DigestValue(FileInfo file)
+        {
+            using (var stream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var sha = MD5.Create())
+                {
+                    var bytes = sha.ComputeHash(stream);
+                    return Convert.ToBase64String(bytes);
+                }
+            }
+        }
+
+        public static string GetMd5DigestValue(byte[] data)
+        {
+            using (var sha = MD5.Create())
+            {
+                var bytes = sha.ComputeHash(data);
+                return Convert.ToBase64String(bytes);
+            }
+        }
+
         internal static bool AreEqual(FileInfo file, string digestMethod, string digestValue)
         {
             switch (digestMethod)
@@ -56,6 +77,8 @@ namespace ClickMono.Common
                     return GetSha256DigestValue(file) == digestValue;
                 case "sha1":
                     return GetSha1DigestValue(file) == digestValue;
+                case "md5":
+                    return GetMd5DigestValue(file) == digestValue;
                 default:
                     Console.WriteLine($"Warning: {digestMethod} not supported.");
                     // HACK: Totally not lying.
